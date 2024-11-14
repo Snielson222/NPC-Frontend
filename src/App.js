5510933056b4d9b4ec3afe97e2c189798260893e
Steven Nielson <snielson222@gmail.com>
2024-11-14 09:51:37 -0500

    added loading orb

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Fantasy NPC Generator</h1>
+      <h1>Fantasy Character Generator</h1>
       <NPCForm onGenerateNPC={generateNPC} />
       {loading ? <LoadingSpinner /> : (
         <>
--- src/components/NPCForm.js
+++ src/components/NPCForm.js
@@ -2,9 +2,6 @@ import React, { useState } from 'react';
 
 const NPCForm = ({ onGenerateNPC }) => {
   const [npcDetails, setNpcDetails] = useState({
-    race: '',
-    classType: '',
-    backstory: '',
     weaponType: '',
     armorStyle: '',
     backgroundScenery: '',
@@ -21,13 +18,10 @@ const NPCForm = ({ onGenerateNPC }) => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" name="race" placeholder="Race" onChange={handleChange} />
-      <input type="text" name="classType" placeholder="Class" onChange={handleChange} />
-      <textarea name="backstory" placeholder="Backstory" onChange={handleChange}></textarea>
       <input type="text" name="weaponType" placeholder="Weapon Type" onChange={handleChange} />
       <input type="text" name="armorStyle" placeholder="Armor Style" onChange={handleChange} />
       <input type="text" name="backgroundScenery" placeholder="Background Scenery" onChange={handleChange} />
-      <button type="submit">Generate NPC</button>
+      <button type="submit">Generate Character</button>
     </form>
   );
 };
