a657ea30bc67a1105061a8c11f5d94886094f7a9
Steven Nielson <snielson222@gmail.com>
2024-11-14 09:41:21 -0500

    npc display

---
+++ src/components/NPCDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NPCDisplay = ({ npc }) => (
+  npc ? (
+    <div>
+      <h2>{npc.name}</h2>
+      <p>Race: {npc.race}</p>
+      <p>Class: {npc.class}</p>
+      <p>Backstory: {npc.backstory}</p>
+    </div>
+  ) : <p>No NPC generated yet.</p>
+);
+
+export default NPCDisplay;
